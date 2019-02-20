523f641a78a3d79a73d9e0bd315779e990dea610
fay <fay@fays-MacBook.local>
2019-02-20 22:03:27 +0800

    adds stats feature

--- src/main/java/com/memorynotfound/springboot/Application.java
+++ src/main/java/com/memorynotfound/springboot/Application.java
@@ -8,6 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import java.lang.management.ManagementFactory;
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+import javax.management.ObjectInstance;
+import java.util.Set;
+import javax.cache.management.CacheStatisticsMXBean;
+import javax.management.MBeanServerInvocationHandler;
 
 @EnableCaching
 @SpringBootApplication
@@ -29,16 +40,17 @@ public class Application implements CommandLineRunner {
     public void run(String... args) throws Exception {
         log.info("Spring Boot Ehcache 2 Caching Example Configuration");
         log.info("using cache manager:!!!!!!!!!!!!!!!!!!!!!!!! " + cacheManager.getClass().getName());
-
+        Object nc = cacheManager.getCache("instruments").getNativeCache();
+        log.info("name!!111 " + nc.getClass().getName());
         musicService.clearCache();
 
-        play("trombone");
+        /**play("trombone");
         play("guitar");
         play("trombone");
         play("bass");
         play("trombone");
         play("guitar");
-        play("bass");
+        play("bass");*/
         play("bass4");
         play("bass4");
         play("bass4");
@@ -46,7 +58,26 @@ public class Application implements CommandLineRunner {
         play("bass5");
         play("bass5");
         play("bass4");
+
+        final MBeanServer beanServer = ManagementFactory.getPlatformMBeanServer();
+
+final Set<ObjectInstance> cacheBeans = beanServer.queryMBeans(
+    ObjectName.getInstance("javax.cache:type=CacheStatistics,CacheManager=*,Cache=*"), null);
+
+for ( ObjectInstance cacheBean : cacheBeans ) {
+    final CacheStatisticsMXBean cacheStatisticsMXBean = MBeanServerInvocationHandler.newProxyInstance(beanServer, cacheBean.getObjectName(), CacheStatisticsMXBean.class, false);
+
+   System.out.println("Gets: " + cacheStatisticsMXBean.getCacheGets());
+    System.out.println("Hits: " + cacheStatisticsMXBean.getCacheHits());
+    System.out.println("Misses: " + cacheStatisticsMXBean.getCacheMisses());
+    System.out.println("Removals: " + cacheStatisticsMXBean.getCacheRemovals());
+    System.out.println("Evictions: " + cacheStatisticsMXBean.getCacheEvictions());
+    System.out.println("Avg Get Time: " + cacheStatisticsMXBean.getAverageGetTime());
+    System.out.println("Avg Put Time: " + cacheStatisticsMXBean.getAveragePutTime());
+    System.out.println("Avg Remove Time: " + cacheStatisticsMXBean.getAverageRemoveTime());
+     
     }
+}
 
     private void play(String instrument){
         log.info("Calling: " + MusicService.class.getSimpleName() + ".play(\"" + instrument + "\");");
